cc8623f64fe71fdcc2796459f18afecd6861a9f9
Sabesh Bharathi <sabesh5601@gmail.com>
2021-08-09 00:11:49 +0530

    Add date tracking for request number

---
+++ Typesense/Sources/Typesense/Shared/RequestNumber.swift
@@ -0,0 +1,18 @@
+//
+//  RequestNumber.swift
+//  
+//
+//  Created by Sabesh Bharathi on 08/08/21.
+//
+
+import Foundation
+
+extension Date {
+    var millisecondsSince1970:Int64 {
+        return Int64((self.timeIntervalSince1970 * 1000.0).rounded())
+    }
+
+    init(milliseconds:Int64) {
+        self = Date(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
+    }
+}
