2ceb75d499f5323ff5fc72cbfebb038ee9d9cac0
Sabesh Bharathi <sabesh5601@gmail.com>
2021-08-25 00:42:42 +0530

    Add `Collections` create and retrieve

--- Sources/Typesense/Api/Collections.swift
+++ Sources/Typesense/Api/Collections.swift
@@ -9,11 +9,26 @@ struct Collections {
         apiCall = ApiCall(config: config)
     }
     
-    func create() -> String {
-        return "Create"
+    mutating func create(schema: CollectionSchema) {
+        var schemaData: Data? = nil
+        do {
+            schemaData = try encoder.encode(schema)
+            apiCall.post(endPoint: "collections", body: schemaData!) { result in
+                print(result as Any)
+            }
+        } catch {
+            print("ERROR: Unable to resolve JSON from schema")
+        }
     }
     
-    func retrieve() -> String {
-        return "Retrieve"
+    mutating func retrieve() {
+        apiCall.get(endPoint: "collections") { result in
+            do {
+                let fetchedCollections = try decoder.decode([Collection].self, from: result!)
+            } catch {
+                print("ERROR: Unable to resolve retrieved collections")
+            }
+            
+        }
     }
 }
--- Tests/ApiTests/CollectionTests.swift
+++ Tests/ApiTests/CollectionTests.swift
@@ -4,10 +4,10 @@ import XCTest
 final class CollectionTests: XCTestCase {
     func testCollections() {
         let newConfig = Configuration(nodes: [Node(host: "localhost", port: "8108", nodeProtocol: "http")], apiKey: "xyz")
-        let myClient = Client(config: newConfig)
+        var myClient = Client(config: newConfig)
         
         //let mySchema = CollectionSchema(name: "Sample", fields: [Field(name: "Field1", type: Field.ModelType.int64, _optional: true, facet: false)], defaultSortingField: "Field1")
         let res = myClient.collections.retrieve()
-        XCTAssertEqual(res, "Retrieve")
+
     }
 }
