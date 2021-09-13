18869eda0c6c00c44ce2a333730e1610d81e14e9
Sabesh Bharathi <sabesh5601@gmail.com>
2021-09-13 05:07:29 -0500

    Merge from initialize-swift-client (#6)

--- Sources/Typesense/Api/ApiCall.swift
+++ Sources/Typesense/Api/ApiCall.swift
@@ -2,6 +2,8 @@ import Foundation
 
 
 let APIKEYHEADERNAME = "X-TYPESENSE-API-KEY"
+let HEALTHY = true
+let UNHEALTHY = false
 
 struct ApiCall {
     var nodes: [Node]
@@ -23,36 +25,39 @@ struct ApiCall {
         self.numRetries = config.numRetries
         self.retryIntervalSeconds = config.retryIntervalSeconds
         self.sendApiKeyAsQueryParam = config.sendApiKeyAsQueryParam
+        
+        self.initializeMetadataForNodes()
     }
     
     
-    mutating func get(endPoint: String, completionHandler: @escaping (Data?) -> ()) {
+
+    mutating func get(endPoint: String, completionHandler: @escaping (Data?, URLResponse?, Error?) -> ()) {
         self.performRequest(requestType: RequestType.get, endpoint: endPoint, completionHandler: completionHandler)
     }
     
-    mutating func delete(endPoint: String, completionHandler: @escaping (Data?) -> ()) {
+    mutating func delete(endPoint: String, completionHandler: @escaping (Data?, URLResponse?, Error?) -> ()) {
         self.performRequest(requestType: RequestType.delete, endpoint: endPoint, completionHandler: completionHandler)
     }
     
-    mutating func post(endPoint: String, body: Data, completionHandler: @escaping (Data?) -> ()) {
+    mutating func post(endPoint: String, body: Data, completionHandler: @escaping (Data?, URLResponse?, Error?) -> ()) {
         self.performRequest(requestType: RequestType.post, endpoint: endPoint, body: body, completionHandler: completionHandler)
     }
     
-    mutating func put(endPoint: String, body: Data, completionHandler: @escaping (Data?) -> ()) {
+    mutating func put(endPoint: String, body: Data, completionHandler: @escaping (Data?, URLResponse?, Error?) -> ()) {
         self.performRequest(requestType: RequestType.put, endpoint: endPoint, body: body, completionHandler: completionHandler)
     }
     
-    mutating func patch(endPoint: String, body: Data, completionHandler: @escaping (Data?) -> ()) {
+    mutating func patch(endPoint: String, body: Data, completionHandler: @escaping (Data?, URLResponse?, Error?) -> ()) {
         self.performRequest(requestType: RequestType.patch, endpoint: endPoint, body: body, completionHandler: completionHandler)
     }
     
-    mutating func performRequest(requestType: RequestType, endpoint: String, body: Data? = nil, completionHandler: @escaping (Data?) -> ()) {
+    mutating func performRequest(requestType: RequestType, endpoint: String, body: Data? = nil, completionHandler: @escaping (Data?, URLResponse?, Error?) -> ()) {
         let requestNumber = Date().millisecondsSince1970
         print("Request #\(requestNumber): Performing \(requestType.rawValue) request: /\(endpoint)")
         
         for numTry in 1...self.numRetries + 1 {
-            let selectedNode = self.getNextNode(requestNumber: requestNumber)
-            print("Request \(requestNumber): Attempting \(requestType.rawValue) request: Try \(numTry) to \(selectedNode)/\(endpoint)")
+            var selectedNode = self.getNextNode(requestNumber: requestNumber)
+            print("Request #\(requestNumber): Attempting \(requestType.rawValue) request: Try \(numTry) to \(selectedNode)/\(endpoint)")
             
             let urlString = uriFor(endpoint: endpoint, node: selectedNode)
             let url = URL(string: urlString)
@@ -64,14 +69,36 @@ struct ApiCall {
             request.httpMethod = requestType.rawValue
 
             if let httpBody = body {
-                request.setValue("application/json", forHTTPHeaderField: "Accept") 
+                request.setValue("application/json", forHTTPHeaderField: "Accept")
                 request.setValue("application/json", forHTTPHeaderField: "Content-Type")
                 request.httpBody = httpBody
             }
             URLSession.shared.dataTask(with: request) { data, response, error in
-
-                if error == nil {
-                    completionHandler(data)
+                if let res = response as? HTTPURLResponse {
+                    if (res.statusCode >= 1 && res.statusCode <= 499) {
+                        // Treat any status code > 0 and < 500 to be an indication that node is healthy
+                        // We exclude 0 since some clients return 0 when request fails
+                        selectedNode.isHealthy = HEALTHY
+                        selectedNode.lastAccessTimeStamp = Date().millisecondsSince1970
+                    }
+                    
+                    if (res.statusCode >= 200 && res.statusCode <= 300) {
+                        //Pass the data and response to completion handler for a 2xx response
+                        completionHandler(data, response, nil)
+                    } else if (res.statusCode < 500) {
+                        //For any response but 500, trigger the completion handler for a static error
+                        completionHandler(data, response, nil)
+                    }
+                    
+                    print("Request \(requestNumber): Request to \(urlString) was made. Response Code was \(res.statusCode)")
+                }
+                
+                if let existingError = error {
+                    //An error is thrown if there is no response, and sequence is retried.
+                    selectedNode.isHealthy = UNHEALTHY
+                    selectedNode.lastAccessTimeStamp = Date().millisecondsSince1970
+                    print("Request \(requestNumber): Request to \(urlString) failed due to error: \(existingError.localizedDescription)")
+                    completionHandler(nil, nil, error)
                 }
             }.resume()
             
@@ -86,23 +113,64 @@ struct ApiCall {
     }
     
     mutating func getNextNode(requestNumber: Int64 = 0) -> Node {
+        
+        //Check if nearest node exists and is healthy
         if let existingNearestNode = nearestNode {
-            print("Nearest Node exists. Updating current node to \(existingNearestNode)")
-            return self.nearestNode!
+            print("Request #\(requestNumber): Nearest Node has health \(existingNearestNode.healthStatus)")
+            
+            if(existingNearestNode.isHealthy || self.nodeDueForHealthCheck(node: existingNearestNode, requestNumber: requestNumber)) {
+                print("Request #\(requestNumber): Updated current node to \(existingNearestNode)")
+                return existingNearestNode
+            }
+            
+            print("Request #\(requestNumber): Falling back to individual nodes")
+        }
+        
+        //Fallback to nodes as usual
+        print("Request #\(requestNumber): Listing health of nodes")
+        let _ = self.nodes.map { node in
+            print("Health of \(node) is \(node.healthStatus)")
         }
         
         var candidateNode = nodes[0]
         
-        print("Nearest Node not found, falling back to nodes")
-        
         for _ in 0...nodes.count {
             currentNodeIndex = (currentNodeIndex + 1) % nodes.count
             candidateNode = self.nodes[currentNodeIndex]
-            print("Falling back to \(candidateNode)")
-            return candidateNode
+            
+            if(candidateNode.isHealthy || self.nodeDueForHealthCheck(node: candidateNode, requestNumber: requestNumber)) {
+                print("Request #\(requestNumber): Updated current node to \(candidateNode)")
+                return candidateNode
+            }
         }
         
         return candidateNode
+    }
+    
+    func nodeDueForHealthCheck(node: Node, requestNumber: Int64 = 0) -> Bool {
+        let isDueForHealthCheck = Date().millisecondsSince1970 - node.lastAccessTimeStamp > (self.healthcheckIntervalSeconds * 1000)
+        if (isDueForHealthCheck) {
+            print("Request #\(requestNumber): \(node) has exceeded healtcheckIntervalSeconds of \(self.healthcheckIntervalSeconds). Adding it back into rotation.")
+        }
+        
+        return isDueForHealthCheck
+    }
+    
+    func setNodeHealthCheck(node: Node, isHealthy: Bool) -> Node {
+        var thisNode = node
+        thisNode.isHealthy = isHealthy
+        thisNode.lastAccessTimeStamp = Date().millisecondsSince1970
+        return thisNode
+    }
+    
+    mutating func initializeMetadataForNodes() {
+        if let existingNearestNode = self.nearestNode {
+            self.nearestNode = self.setNodeHealthCheck(node: existingNearestNode, isHealthy: HEALTHY)
+        }
+        
+        for i in 0..<self.nodes.count {
+            self.nodes[i] = self.setNodeHealthCheck(node: self.nodes[i], isHealthy: HEALTHY)
+        }
     }
     
 }
--- Tests/ApiTests/ApiCallTests.swift
+++ Tests/ApiTests/ApiCallTests.swift
@@ -3,14 +3,139 @@ import XCTest
 
 final class ApiCallTests: XCTestCase {
     func testHealth() {
+            var apiCall = ApiCall(config: Configuration(nodes: [Node(host: "localhost", port: "8108", nodeProtocol: "http")], apiKey: "xyz"))
+            var expectation:XCTestExpectation? = expectation(description: "Check health of system")
+            
+            apiCall.get(endPoint: "health") { result, response, error in
+                do {
+                    if let healthRes = result {
+                        let jsonRes = try decoder.decode(HealthStatus.self, from: healthRes)
+                        XCTAssertTrue(jsonRes.ok)
+                        expectation?.fulfill()
+                        expectation = nil
+                    } else {
+                        print("Response data was nil")
+                    }
+                } catch {
+                    print("Could not resolve health status from response")
+                }
+                
+            }
+            
+            waitForExpectations(timeout: 5, handler: nil)
+        }
+    
+    func testHTTP200() {
         var apiCall = ApiCall(config: Configuration(nodes: [Node(host: "localhost", port: "8108", nodeProtocol: "http")], apiKey: "xyz"))
-        var expectation:XCTestExpectation? = expectation(description: "Check health of system")
+        var expectation:XCTestExpectation? = expectation(description: "Check HTTP 200 Status")
         
-        apiCall.get(endPoint: "health") { result in
-            let jsonRes = try! decoder.decode(HealthStatus.self, from: result!)
-            XCTAssertTrue(jsonRes.ok)
-            expectation?.fulfill()
-            expectation = nil
+        //Test health - should return 200 if healthy
+        apiCall.get(endPoint: "health") { result, response, error in
+            do {
+                if let healthRes = result {
+                    let jsonRes = try decoder.decode(HealthStatus.self, from: healthRes)
+                    XCTAssertTrue(jsonRes.ok)
+                    XCTAssertNil(error)
+                    
+                    if let res = response as? HTTPURLResponse {
+                        //Check 200 ok
+                        XCTAssertEqual(res.statusCode, 200)
+                    }
+                    expectation?.fulfill()
+                    expectation = nil
+                } else {
+                    print("Response data was nil")
+                }
+                
+            } catch {
+                print("Could not resolve health status from response")
+            }
+            
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testHTTP404() {
+        var apiCall = ApiCall(config: Configuration(nodes: [Node(host: "localhost", port: "8108", nodeProtocol: "http")], apiKey: "xyz"))
+        var expectation:XCTestExpectation? = expectation(description: "Check HTTP 404 Status")
+        
+        //Test for a random url - /randomURL that doesn't exist
+        apiCall.get(endPoint: "randomURL") { result, response, error in
+            do {
+                if let notFoundRes = result {
+                    let jsonRes = try decoder.decode(ApiResponse.self, from: notFoundRes)
+                    XCTAssertEqual(jsonRes.message, "Not Found")
+                    XCTAssertNil(error)
+                    
+                    if let res = response as? HTTPURLResponse {
+                        //Check 404 not found
+                        XCTAssertEqual(res.statusCode, 404)
+                    }
+                    expectation?.fulfill()
+                    expectation = nil
+                } else {
+                    print("Response data was nil")
+                }
+                
+            } catch {
+                print("Could not resolve APIResponse type from given response")
+            }
+            
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testHTTP401() {
+        //Initialize with a bad api key
+        var apiCall = ApiCall(config: Configuration(nodes: [Node(host: "localhost", port: "8108", nodeProtocol: "http")], apiKey: "abc"))
+        var expectation:XCTestExpectation? = expectation(description: "Check HTTP 401 Status")
+        
+        apiCall.get(endPoint: "collections") { result, response, error in
+            do {
+                if let notAuthRes = result {
+                    let jsonRes = try decoder.decode(ApiResponse.self, from: notAuthRes)
+                    //Check if bad api key fails while trying to access /collections
+                    XCTAssertEqual(jsonRes.message, "Forbidden - a valid `x-typesense-api-key` header must be sent.")
+                    XCTAssertNil(error)
+                    
+                    if let res = response as? HTTPURLResponse {
+                        //Check 401 unauthorized
+                        XCTAssertEqual(res.statusCode, 401)
+                    }
+                    expectation?.fulfill()
+                    expectation = nil
+                } else {
+                    print("Response data was nil")
+                }
+            } catch {
+                print("Could not resolve APIResponse type from given response")
+            }
+            
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testServerNotFound() {
+        //Initialize unknown node (port 8000)
+        var apiCall = ApiCall(config: Configuration(nodes: [Node(host: "localhost", port: "8000", nodeProtocol: "http")], apiKey: "xyz"))
+        var expectation:XCTestExpectation? = expectation(description: "Check if Server could be found")
+        
+        apiCall.get(endPoint: "health") { result, response, error in
+            
+            if let existingErr = error {
+                let errorMessage = existingErr.localizedDescription
+                //There's no response, just an error message
+                XCTAssertEqual(errorMessage, "Could not connect to the server.")
+                XCTAssertNil(result)
+                XCTAssertNil(response)
+                expectation?.fulfill()
+                expectation = nil
+            } else {
+                print("No Error triggered")
+            }
         }
         
         waitForExpectations(timeout: 5, handler: nil)
