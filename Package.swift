e872467bf2800886f8dcc9b19219367dd9e87f93
programVeins <sabesh5601@gmail.com>
2021-12-28 23:54:39 +0530

    Update minimum deployment to iOS 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Typesense",
     platforms: [
-        .iOS(.v15), .macOS(.v12)
+        .iOS(.v13), .macOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
