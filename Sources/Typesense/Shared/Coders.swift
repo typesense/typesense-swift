7199650faa5171ff2c50d1b05800de15eb546612
programVeins <sabesh5601@gmail.com>
2022-01-04 16:27:51 +0530

    Extend URLSession to use `<15.0` network API

--- Sources/Typesense/Shared/Coders.swift
+++ Sources/Typesense/Shared/Coders.swift
@@ -43,3 +43,21 @@ public struct SlowRequest: Codable {
         case logSlowRequestsTimeMs = "log-slow-requests-time-ms"
     }
 }
+
+@available(iOS, deprecated: 15.0, message: "Use the built-in API instead")
+extension URLSession {
+    func data(for req: URLRequest) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: req) { data, response, error in
+                guard let data = data, let response = response else {
+                    let error = error ?? URLError.invalidURL
+                    return continuation.resume(throwing: error)
+                }
+
+                continuation.resume(returning: (data, response))
+            }
+
+            task.resume()
+        }
+    }
+}
