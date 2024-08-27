6cf85ff0d0523106bff1e836768a80d599576f00
Hayden <hayden.hhoang@gmail.com>
2024-08-27 07:25:08 +0700

    update synonym schema

--- Sources/Typesense/Models/SearchSynonym.swift
+++ Sources/Typesense/Models/SearchSynonym.swift
@@ -16,17 +16,25 @@ public struct SearchSynonym: Codable {
     /** Array of words that should be considered as synonyms. */
     public var synonyms: [String]
     public var _id: String
+    /** Locale for the synonym, leave blank to use the standard tokenizer. */
+    public var locale: String?
+    /** By default, special characters are dropped from synonyms. Use this attribute to specify which special characters should be indexed as is. */
+    public var symbolsToIndex: [String]?
 
-    public init(root: String? = nil, synonyms: [String], _id: String) {
+    public init(root: String? = nil, synonyms: [String], _id: String, locale: String? = nil, symbolsToIndex: [String]? = nil) {
         self.root = root
         self.synonyms = synonyms
         self._id = _id
+        self.locale = locale
+        self.symbolsToIndex = symbolsToIndex
     }
 
-    public enum CodingKeys: String, CodingKey { 
+    public enum CodingKeys: String, CodingKey {
+        case _id = "id"
         case root
         case synonyms
-        case _id = "id"
+        case locale
+        case symbolsToIndex = "symbols_to_index"
     }
 
 }
