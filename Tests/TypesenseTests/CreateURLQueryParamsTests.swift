4f9d06b48335500127a8cdd4c910aaceabded01d
Hayden <hayden.hhoang@gmail.com>
2024-11-05 22:12:44 +0700

    fix: integer query parameters value 1 and 0 being converted to boolean

--- Tests/TypesenseTests/CreateURLQueryParamsTests.swift
+++ Tests/TypesenseTests/CreateURLQueryParamsTests.swift
@@ -6,18 +6,24 @@ final class CreateURLQueryParamsTests: XCTestCase {
         var name = "Fast"
         var age: Int = 20
         var price: Int64 = 2000
+        var page: Int? = 1
+        var groupLimit: Int64? = 0
         var speed: Double = 120.555
         var mileage: Float = 120.5
         var isElectric = true
+        var inStock: Bool = false
         var empty: String? = nil
 
         enum CodingKeys: String, CodingKey {
             case name
             case age
             case price
+            case page
+            case groupLimit = "group_limit"
             case speed
             case mileage
             case isElectric = "is_electric"
+            case inStock = "in_stock"
             case empty
         }
     }
@@ -25,13 +31,16 @@ final class CreateURLQueryParamsTests: XCTestCase {
         do {
             let queryParams = try createURLQuery(forSchema: Car())
             print(queryParams)
-            XCTAssertEqual(queryParams.count, 6)
+            XCTAssertEqual(queryParams.count, 9)
             XCTAssertTrue(queryParams.contains(URLQueryItem(name: "name", value: "Fast")))
             XCTAssertTrue(queryParams.contains(URLQueryItem(name: "age", value: "20")))
             XCTAssertTrue(queryParams.contains(URLQueryItem(name: "price", value: "2000")))
+            XCTAssertTrue(queryParams.contains(URLQueryItem(name: "page", value: "1")))
+            XCTAssertTrue(queryParams.contains(URLQueryItem(name: "group_limit", value: "0")))
             XCTAssertTrue(queryParams.contains(URLQueryItem(name: "speed", value: "120.555")))
             XCTAssertTrue(queryParams.contains(URLQueryItem(name: "mileage", value: "120.5")))
             XCTAssertTrue(queryParams.contains(URLQueryItem(name: "is_electric", value: "true")))
+            XCTAssertTrue(queryParams.contains(URLQueryItem(name: "in_stock", value: "false")))
             XCTAssertFalse(queryParams.contains(URLQueryItem(name: "empty", value: "null")))
         } catch (let error) {
             print(error)
